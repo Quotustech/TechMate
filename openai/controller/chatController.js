73ddf9d18fe54d5f069d9a52ec4f6ca6859f3008
Abiksoumya <soumyamohanty540@gmail.com>
2023-08-22 12:34:31 +0530

    auth responce:

--- openai/controller/chatController.js
+++ openai/controller/chatController.js
@@ -1,62 +1,58 @@
 
 const Chat = require('../model/chatModel');
 const User = require('../model/userModel');
-const apiKey = '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';
+const axios = require('axios')
+const apiKey = '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';
 
 
 
 
-const sendMessageToChatGPT = async (req, res) =>{
-    try{
+const sendMessageToChatGPT = async (req, res) => {
+    try {
         const { userId, message } = req.body;
+        console.log(userId, message)
         const user = await User.findById(userId);
-        if(!user){
+        if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
-        try{
-            const response = await axios.post(
-                'https://api.openai.com/v1/chat/completions',
-                {
-                  model: 'gpt-3.5-turbo',
-                  messages: [
+        const response = await axios.post(
+            'https://api.openai.com/v1/chat/completions',
+            {
+                model: 'gpt-3.5-turbo',
+                messages: [
                     {
-                      role: 'system',
-                      content: 'You are a helpful assistant.',
+                        role: 'system',
+                        content: 'You are a helpful assistant.',
                     },
                     {
-                      role: 'user',
-                      content: message,
+                        role: 'user',
+                        content: message,
                     },
-                  ],
-                },
-                {
-                  headers: {
+                ],
+            },
+            {
+                headers: {
                     'Content-Type': 'application/json',
                     Authorization: `Bearer ${apiKey}`,
-                  },
-                }
-              );
-              console.log("this is the responce result",response.data)
-              const newChat = new Chat({ user: userId, question: message, answer:response.data.choices[0].message.content });
-              await newChat.save();
-              return res.json({ question: message, answer:response.data.choices[0].message.content });
-              
-
-        }
-        catch (error) {
-            console.log(error)
-            const customError = new Error(' occurred while processing the request',error);
-            customError.statusCode = 500; // Set the desired status code here
-            throw customError;
-
-        }
-
+                },
+            }
+        );
+        console.log("this is the responce result", response.data.choices[0].message.content)
+        const newChat = new Chat({ user: userId, question: message, answer: response.data.choices[0].message.content });
+        await newChat.save();
+        return res.json({question :message,response:response.data.choices[0].message.content});
     }
-    catch (error){
-        console.log(error);
-        res.status(500).json({ error: 'Internal server error' });
+    catch (error) {
+        res.status(error.response.status).json(
+            { error_code :error.code,
+              error_status_code: error.response.status,
+             error_status_texte: error.response.statusText });
     }
 
-}
+};
+
+module.exports = {
+    sendMessageToChatGPT
+};
 
 
--- openai/routes/router.js
+++ openai/routes/router.js
@@ -2,8 +2,14 @@ const express = require('express')
 const router = express.Router();
 
 const userController = require('../controller/userController')
+const chatController = require('../controller/chatController')
 // Create user
 router.post('/createuser',userController.createUser)
+
+
+// Create chat
+router.post('/chat',chatController.sendMessageToChatGPT)
+
 
 
 
